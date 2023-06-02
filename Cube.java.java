fdf2de12f232337bb82b0ff8ac33c1128298a6a8
unknown <poonamsurashe0@gmail.com>
2023-06-02 18:03:40 +0530

    cubejava

---
+++ Cube.java.java
@@ -0,0 +1,11 @@
+//program for cube of no.
+
+import java.lang.*;
+class cube
+{
+public static void main(strig args[]);
+int num=2,c;
+c=num*num*num;
+system.out.println("\n cube of"+num "is="c);
+}
+}
